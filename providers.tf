dc0e6fcddc85887c3f616f4af3110f045853933a
JuneMG <me@HEYJUNEGREEN.COM>
2026-03-07 23:31:54 -0500

    Migrate state to Azure backend

--- providers.tf
+++ providers.tf
@@ -5,6 +5,13 @@ terraform {
       version = "~> 3.0"
     }
   }
+
+  backend "azurerm" {
+    resource_group_name  = "rg-cloud-resume-James"
+    storage_account_name = "resumejamesgreenjr"
+    container_name       = "tfstate"
+    key                  = "terraform.tfstate"
+  }
 }
 
 provider "azurerm" {
